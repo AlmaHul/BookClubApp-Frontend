1962aca9d72ed5ad285152b4d9930270e102c28a
ZhuXiaohua11 <80550652+ZhuXiaohua11@users.noreply.github.com>
2025-04-01 11:29:04 +0200

    app

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,20 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import LoginPage from './pages/LoginPage';
 import RegisterPage from './pages/RegisterPage';
+import BooksPage from './pages/BooksPage';
+import Navbar from './components/Navbar';
 
 function App() {
+  const token = localStorage.getItem("token");
+
   return (
     <Router>
+      {/* Only show navbar if token exists (user is logged in) */}
+      {token && <Navbar />}
       <Routes>
         <Route path="/" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
+        <Route path="/books" element={<BooksPage />} />
       </Routes>
     </Router>
   );
